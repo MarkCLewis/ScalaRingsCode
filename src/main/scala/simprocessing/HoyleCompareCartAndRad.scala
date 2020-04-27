ffeb81ecd38e03dbd2bd5f8dc074eb78dd95ec5e
Gracen Hoyle <ghoyle@trinity.edu>
2020-04-27 09:47:53 -0500

    Changed build.sbt to remove reference to java home. Added CompareCartAndRad.scala for comparing CartAndRad files.

---
+++ src/main/scala/simprocessing/HoyleCompareCartAndRad.scala
@@ -0,0 +1,23 @@
+package simprocessing
+
+import java.io.File
+import java.io.FilenameFilter
+import data.CartAndRad
+import util.Particle
+import util.GCCoord
+import scala.sys
+
+object CompareCartAndRad extends App {
+    println(args.length)
+    if (args.length < 2) {
+        println("ERROR: Must have two CartAndRad files to compare.")
+        sys.exit(0)
+    } else {
+        // Took these lines from ProcessCartAndRad.scala to process the files
+        println("Starting up")
+        val files = new File(args(0)).listFiles((d: File, s: String) => s.startsWith("CartAndRad"))
+        println(files.length)
+
+    }
+
+}
