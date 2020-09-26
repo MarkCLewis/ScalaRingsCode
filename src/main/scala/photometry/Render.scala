aee8223b4c3049974d1aad2fdf7185aec62d9285
Mark Lewis <mlewis@trinity.edu>
2020-09-26 12:03:46 -0500

    some refactoring on photometry to break up the rendering.

--- src/main/scala/photometry/Render.scala
+++ src/main/scala/photometry/Render.scala
@@ -18,18 +18,26 @@ object Render {
     val carURL = new URL("http://www.cs.trinity.edu/~mlewis/Rings/AMNS-Moonlets/Moonlet4/CartAndRad." + step.toString + ".bin")
 		val geom = new KDTreeGeometry[BoundingSphere](data.CartAndRad.readStream(carURL.openStream).map(p => new ScatterSphereGeom(Point(p.x, p.y, p.z), p.rad, _ => new RTColor(1, 1, 1, 1), _ => 0.0)))
     val light = PointLight(RTColor(1, 1, 1), Point(1, 0, 0.2), Set.empty)
-    val viewLoc = Point(0, 0, 1e-5)
+    val viewLoc = Point(0, 0, 2e-5)
     val forward = Vect(0, 0, 1)
     val up = Vect(0, 1, 0)
     val bimg = new BufferedImage(1200, 1200, BufferedImage.TYPE_INT_ARGB)
     val img = new rendersim.RTBufferedImage(bimg)
 
-    render(geom, light, viewLoc, forward, up, img, 100000000)
     val frame = new MainFrame {
       title = "Dust Frame"
       contents = new Label("", Swing.Icon(bimg), Alignment.Center)
     }
     frame.visible = true
+
+    var totalPixels: Array[Array[RTColor]] = null
+    for (i <- 1 to 100000) {
+      println(i)
+      val pixels = render(geom, light, viewLoc, forward, up, img, 100000)
+      if (totalPixels == null) totalPixels = pixels else totalPixels = addPixels(totalPixels, pixels)
+      writeToImage(totalPixels, img)
+      frame.repaint()
+    }
   }
 
   def render(
@@ -41,7 +49,7 @@ object Render {
       image: RTImage,
       //pixels: Array[Array[RTColor]],
       numPhotons: Long
-  ): Unit = {
+  ): Array[Array[RTColor]] = {
     val interRect = geom.boundingBox
     val (xmin, xmax, ymin, ymax) =
       (interRect.min.x, interRect.max.x, interRect.min.y, interRect.max.y)
@@ -60,12 +68,13 @@ object Render {
       val iDataOpt = geom.intersect(ray)
       iDataOpt.foreach { iData =>
         val interPoint = iData.point + iData.norm*1e-8
-        val inRay = (viewLoc - interPoint).normalize
         if (geom.intersect(Ray(interPoint, viewLoc)).isEmpty) {
+          val inRay = (viewLoc - interPoint).normalize
           val scatter = iData.geom.asInstanceOf[ScatterGeometry].fractionScattered(ray.dir, inRay, iData)
           if (scatter > 0.0) {
-            val px = ((-inRay.dot(right) / 0.707 + 1.0) * image.width / 2).toInt
-            val py = ((inRay.dot(up) / 0.707 + 1.0) * image.height / 2).toInt
+            val fracForward = inRay dot forward
+            val px = ((inRay.dot(right)/fracForward / 0.707 + 1.0) * image.width / 2).toInt
+            val py = ((inRay.dot(up)/fracForward / 0.707 + 1.0) * image.height / 2).toInt
             if (px >= 0 && px < image.width && py >= 0 && py < image.height) {
               pixels(px)(py) += light.col * scatter
             }
@@ -73,7 +82,14 @@ object Render {
         }
       }
     }
-    
+    pixels
+  }
+
+  def addPixels(pixels1: Array[Array[RTColor]], pixels2: Array[Array[RTColor]]): Array[Array[RTColor]] = {
+    Array.tabulate(pixels1.length, pixels1(0).length)((i, j) => pixels1(i)(j) + pixels2(i)(j))
+  }
+
+  def writeToImage(pixels: Array[Array[RTColor]], image: RTImage): Unit = {
     val maxPix = pixels.foldLeft(0.0)((m,row) => m max row.foldLeft(0.0)((m2, p) => m2 max p.r max p.g max p.b))
     for (px <- 0 until image.width; py <- 0 until image.height) {
       image.setColor(px, py, (pixels(px)(py) / maxPix).copy(a = 1.0))
