8a3ea05b238fe087a851489e24e0993aa8655a91
eruetsch <eruetsch@trinity.edu>
2020-10-17 11:32:27 -0500

    attempting to add dust to area of collision

--- src/main/scala/photometry/Render.scala
+++ src/main/scala/photometry/Render.scala
@@ -18,27 +18,31 @@ import data.HighVelocityCollisions
 object Render {
   def main(args: Array[String]): Unit = {
     val step = 10000
-//    val carURL = new URL("http://www.cs.trinity.edu/~mlewis/Rings/AMNS-Moonlets/HighRes/Moonlet4d/CartAndRad." + step.toString + ".bin")
-    val carURL = new URL("http://www.cs.trinity.edu/~mlewis/Rings/MesoScaleFeatures/AGUPosterRun/a=123220:q=2.8:min=1.5e-9:max=1.5e-8:rho=0.4:sigma=45.5/CartAndRad.4420.bin")
-//    val impactURL = new URL("http://www.cs.trinity.edu/~mlewis/Rings/AMNS-Moonlets/HighRes/Moonlet4d/HighVelColls.bin")
+    val carURL = new URL("http://www.cs.trinity.edu/~mlewis/Rings/AMNS-Moonlets/HighRes/Moonlet4d/CartAndRad." + step.toString + ".bin")
+//    val carURL = new URL("http://www.cs.trinity.edu/~mlewis/Rings/MesoScaleFeatures/AGUPosterRun/a=123220:q=2.8:min=1.5e-9:max=1.5e-8:rho=0.4:sigma=45.5/CartAndRad.4420.bin")
+    val impactURL = new URL("http://www.cs.trinity.edu/~mlewis/Rings/AMNS-Moonlets/HighRes/Moonlet4d/HighVelColls.bin")
     val ringGeom = new KDTreeGeometry[BoundingBox](data.CartAndRad.readStream(carURL.openStream)
       .filter(p => p.y < 2e-5 && p.y > -2e-5)
       .map(p => new ScatterSphereGeom(Point(p.x, p.y, p.z), p.rad, _ => new RTColor(1, 1, 1, 1), _ => 0.0)), 5, BoxBoundsBuilder)
-  //  val impacts = HighVelocityCollisions.readStream(impactURL.openStream()).takeRight(100)
-  //  println(impacts.last)
-  //  println(impacts.foldLeft(0)(_ + _.colls.length))
-  //  val impactGeom = new KDTreeGeometry[BoundingSphere](impacts.flatMap(scd => scd.colls
-    //   .map(coll => new ScatterSphereGeom(Point(coll.p1.x, coll.p1.y, coll.p1.z), coll.p1.rad*100 min 1e-7, 
-    //   _ => new RTColor(coll.vel/5e-6 min 1.0, 0, 1.0 - coll.vel/5e-6 min 1.0, 1), _ => 0.0))))
-    // val dustGeom = new DustGeom(Point(0,0,0), Vect(5e-6, 0, 0), Vect(0, 2e-5, 0), Vect(0, 0, 1e-7), 0.5/1e-6)
-    val geom = new ListScene(ringGeom) //, dustGeom, impactGeom)
+    val impacts = HighVelocityCollisions.readStream(impactURL.openStream()).takeRight(100)
+    //println(impacts.last)
+    //println(impacts.foldLeft(0)(_ + _.colls.length))
+    val impactGeom = new KDTreeGeometry[BoundingSphere](impacts.flatMap(scd => scd.colls
+       .map(coll => new ScatterSphereGeom(Point(coll.p1.x, coll.p1.y, coll.p1.z), coll.p1.rad*100 min 1e-7, 
+       _ => new RTColor(coll.vel/5e-6 min 1.0, 0, 1.0 - coll.vel/5e-6 min 1.0, 1), _ => 0.0))))
+       
+//    val dustGeom = new DustGeom(Point(0,0,0), Vect(5e-6, 0, 0), Vect(0, 2e-5, 0), Vect(0, 0, 1e-7), 0.5/1e-6)
+    val dustGeom = new KDTreeGeometry[BoundingSphere](impacts.flatMap(scd => scd.colls
+       .map(coll => new DustGeom(Point(coll.p1.x, coll.p1.y, coll.p1.z), Vect(coll.p1.rad*100 min 1e-7, 0, 0), Vect(0, coll.p1.rad*100 min 1e-7, 0), Vect(0, 0, coll.p1.rad*100 min 1e-7), 0.5/1e-6))))
+
+    val geom = new ListScene(ringGeom, dustGeom, impactGeom)
     val lights = List(PhotonSource(PointLight(RTColor(1, 1, 1), Point(1, 0, 0.2), Set.empty), 100000), PhotonSource(PointLight(new RTColor(1.0, 0.8, 0.2), Point(-1e-1, 0, 1e-2)), 20000))
     val viewLoc = Point(0, 0, 7e-6)
     val forward = Vect(0, 0, 1)
     val up = Vect(0, 1, 0)
     val bimg = new BufferedImage(1200, 1200, BufferedImage.TYPE_INT_ARGB)
     val img = new rendersim.RTBufferedImage(bimg)
-    val threads: Int = 24
+    val threads: Int = 1
 
     val frame = new MainFrame {
       title = "Dust Frame"
