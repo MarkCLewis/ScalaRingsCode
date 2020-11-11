c131e2a08217bdc99a26f7b5052e83fda699517b
Keaton Holt <kholt@trinity.edu>
2020-11-11 15:44:47 -0600

    More on the statistical redistribution

--- src/main/scala/simprocessing/PreparePWPSMod.scala
+++ src/main/scala/simprocessing/PreparePWPSMod.scala
@@ -2,11 +2,22 @@ package simprocessing
 
 import data.CartAndRad
 import util.Particle
+import util.GCCoord
 
 import scala.math
+import scala.collection.mutable
 
 object PreparePWPSMod {
 	def main(args: Array[String]): Unit = {
+        val TESTING = true
+        if(TESTING){
+            val xs = Seq(0, 3, 6.5, 7, 10)
+            val ys = Seq(0, 1, 5, 5, 10)
+            val ps = (0 to 4 by 1).map(i => GCCoord(xs(i),ys(i),0,0,0,0,1))
+            val cells = getCellStats(ps,1,2,1)
+            cells.foreach(l => l.foreach(c => println(c.volume())))
+            sys.exit()
+        }
         // if (args.contains("-help") || args.length < 1) {
         //     println("Arguments:")
         //     println("\t-dir path: path to directory, defaults to current directory")
@@ -28,17 +39,83 @@ object PreparePWPSMod {
         // val azMin = args.sliding(2).find(_(0) == "-azMin").map(_(1).toDouble).getOrElse(Double.MinValue)
         // val azMax = args.sliding(2).find(_(0) == "-azMax").map(_(1).toDouble).getOrElse(Double.MaxValue)
 
-        // val particles = CartAndRad.read(new File(dir, "CartAndRad."+step+".bin"))
-
+        // read in the particles
+        //val particles = CartAndRad.read(new File(dir, "CartAndRad."+step+".bin"))
+        
         // //modify the particles in some way
-        // //return the modified particles
-        // val newParticles = particles
+        
 
+        // return the modified particles
         // CartAndRad.write(new File(dir, "CartAndRadMod."+step+".bin"), newParticles)
-        val p = Particle(0,0,0,0,0,0,1)
-        twoSizeCubeExpand(p)
     }
 
+    def getCellStats(gcParticles: Seq[GCCoord], xBins: Int, yBins: Int, pMass: Double): Array[Array[Cell]] = {
+        val xValues = gcParticles.map(p => p.X)
+        val yValues = gcParticles.map(p => p.Y)
+        val xMin = xValues.min
+        val xMax = xValues.max
+        val yMin = yValues.min
+        val yMax = yValues.max
+        val xSpacing = (xMax-xMin)/xBins
+        val ySpacing = (yMax-yMin)/yBins
+
+        val divPart = Array.fill(xBins,yBins)(mutable.ArrayBuffer[GCCoord]())
+        for(p <- gcParticles){
+            var i = 0
+            var counted = false
+            while(i < xBins && !counted){
+                var j = 0
+                while(j < yBins && !counted){
+                    val x0 = xMin+i*xSpacing
+                    val y0 = yMin+j*ySpacing
+                    if(p.X >= x0 && (p.X < x0+xSpacing || i == xBins-1 && p.X == x0+xSpacing) && p.Y >= y0 && (p.Y < y0+ySpacing ||
+                    j == yBins-1 && p.Y == y0+ySpacing)){
+                        divPart(i)(j) += p
+                        counted = true
+                    }
+                    j += 1
+                }
+                i += 1
+            }
+        }
+        //for(i <- 0 until xBins){ for(j <- 0 until yBins){ println(divPart(i)(j).length)} }
+
+        val grid = Array.ofDim[Cell](xBins,yBins)
+        for(i <- 0 until xBins){
+            for(j <- 0 until yBins){
+                val x0 = xMin + i*xSpacing
+                val y0 = yMin + j*ySpacing
+                val cellParticles = divPart(i)(j)
+                val pRad = cellParticles(0).rad
+                val X = meanAndVariance(cellParticles.map(p => p.X))
+                val Y = meanAndVariance(cellParticles.map(p => p.Y))
+                val e = meanAndVariance(cellParticles.map(p => p.e))
+                val phi = meanAndVariance(cellParticles.map(p => p.phi))
+                val I = meanAndVariance(cellParticles.map(p => p.i))
+                val zeta = meanAndVariance(cellParticles.map(p => p.zeta))
+                grid(i)(j) = Cell(x0,y0,xSpacing,ySpacing,cellParticles.length,pRad,pMass,X,Y,e,phi,I,zeta)
+            }
+        }
+
+        grid
+    }
+
+    def meanAndVariance(set: Seq[Double]): (Double, Double) = {
+        val mean = set.sum/set.length
+        val variance = set.map(x => x*x).sum/set.length - mean*mean
+        (mean, variance)
+    }
+    
+    case class Cell(x0: Double, y0: Double, xSz:Double, ySz: Double, pcnt: Int, pRad: Double, pMass: Double, X: (Double, Double), 
+        Y: (Double, Double), e: (Double, Double), phi: (Double, Double), I: (Double, Double), zeta: (Double, Double)){
+            def volume(): Double = pcnt * (4.0/3)*math.Pi*math.pow(pRad,3)
+            def mass(): Double = pcnt * pMass
+        }
+    
+    
+    
+    
+    
     def twoSizeCubeExpand(p: Particle): Seq[Particle] = {
         val newParticles = Array.ofDim[Particle](11)
         val r0 = p.rad
