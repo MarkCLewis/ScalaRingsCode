d02f5adb0198482fa28ba092659541291e7ffb25
Mark Lewis <mlewis@trinity.edu>
2020-11-18 15:58:33 -0600

    Added iterative isolate.

--- src/main/scala/simprocessing/IsolateCartCluster.scala
+++ src/main/scala/simprocessing/IsolateCartCluster.scala
@@ -1,14 +1,37 @@
 package simprocessing
+
 import data.CartAndRad
 import util.Particle
+import collection.mutable
 
 object IsolateCartCluster{
   def main(args: Array[String]): Unit = {
+    if (args.length < 4) {
+      println("Usage: infile outfile (-dist mult | -iter mult")
+      return
+    }
     val inFile = args(0)
     val outFile = args(1)
+    val mult = args(3).toDouble
     val particles = CartAndRad.read(new java.io.File(inFile))
     val bigBoy = particles.maxBy(_.rad)
-    val survivors = particles.filter(p => distance(p, bigBoy) < 1.5*bigBoy.rad)
+    val survivors = if (args(2) == "-dist") {
+      particles.filter(p => distance(p, bigBoy) < 1.5*bigBoy.rad)
+    } else {
+      val keep = mutable.Buffer[Particle](bigBoy)
+      val rest = mutable.Buffer[Particle](particles.filter(_ != bigBoy):_*)
+      var done = false
+      while (!done) {
+        val close = mutable.Buffer[Particle]()
+        for (r <- rest; k <- keep) {
+          if (distance(r, k) < mult * (r.rad + k.rad)) close += r
+        }
+        rest --= close
+        keep ++= close
+        done = close.isEmpty
+      }
+      keep.toIndexedSeq
+    }
     CartAndRad.write(new java.io.File(outFile), survivors)
   }
   def distance(part1:Particle, part2:Particle): Double = {
