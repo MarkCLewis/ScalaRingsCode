c63e476297a1ab61bc4abbc583730c0c8e8447bb
Mark Lewis <mlewis@trinity.edu>
2020-12-02 15:44:21 -0600

    Worked on setting up collisions.

--- src/main/scala/simprocessing/SetUpCollision.scala
+++ src/main/scala/simprocessing/SetUpCollision.scala
@@ -11,16 +11,22 @@ object SetUpCollision{
     }
     val inFile = args(0)
     val outFile = args(1)
-    val x = args(2)
-    val y = args(3)
-    val z = args(4)
-    val vx = args(5)
-    val vy = args(6)
-    val vz = args(7)
+    val dx = args(2).toDouble
+    val dy = args(3).toDouble
+    val dz = args(4).toDouble
+    val dvx = args(5).toDouble
+    val dvy = args(6).toDouble
+    val dvz = args(7).toDouble
     val particles = CartAndRad.read(new java.io.File(inFile))
     val bigBoy = particles.maxBy(_.rad)
-
-  }  
+    val centeredParticles = particles.map { case Particle(x, y, z, vx, vy, vz, r) => 
+      Particle(x - bigBoy.x, y - bigBoy.y, z - bigBoy.z, vx - bigBoy.vx, vy - bigBoy.vy, vz - bigBoy.vz, r)}
+    val cluster1 = centeredParticles.map { case Particle(x, y, z, vx, vy, vz, r) =>
+      Particle(x + dx, y + dy, z + dz, vx + dvx, vy + dvy, vz + dvz, r)}
+    val cluster2 = centeredParticles.map { case Particle(x, y, z, vx, vy, vz, r) =>
+      Particle(x - dx, y - dy, z - dz, vx - dvx, vy - dvy, vz - dvz, r)}
+    CartAndRad.write(new java.io.File(outFile), cluster1 ++ cluster2)
+  }
 }
 
 //takes infile outfile -- done
