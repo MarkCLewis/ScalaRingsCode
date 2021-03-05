50e43d0a242434d721adc8e7c8689e245c951c92
Mark Lewis <mlewis@trinity.edu>
2021-03-05 13:01:17 -0600

    Made the plots more interesting and output to file.

--- src/main/scala/photometry/FigureOutDust.scala
+++ src/main/scala/photometry/FigureOutDust.scala
@@ -9,32 +9,54 @@ import java.io.File
 object FigureOutDust extends App {
   val labelFormat = "%1.3e"
   val FileRegex = """.*CartAndRad.(\d+).bin""".r
-  val cartStep = "CartAndRad.1000.bin"
   val dataDir = new File("/home/mlewis/workspaceResearch/RegolithImpactSims/ImpactTesting/Reg/")
-  for (simDir <- dataDir.listFiles()) {
-    val file = new File(simDir, cartStep)
-    if (file.exists()) {
+  for (simDir <- dataDir.listFiles(); if simDir.isDirectory()) {
+    val dataFiles = simDir.listFiles().filter(_.getName().startsWith("CartAndRad"))
+    val data0 = CartAndRad.read(new File(simDir, "CartAndRad.0.bin"))
+    val rad0 = data0.map(_.rad * 2)
+    val scatter0 = ScatterStyle(
+      data0.map(_.x),
+      data0.map(_.y),
+      EllipseLine,
+      rad0,
+      rad0,
+      PlotSymbol.Sizing.Scaled,
+      PlotSymbol.Sizing.Scaled,
+      GreenARGB
+    )
+    val vector0 = ScatterStyle(
+      data0.flatMap(p => Array(p.x, p.x + p.vx)),
+      data0.flatMap(p => Array(p.y, p.y + p.vy)),
+      NoSymbol,
+      colors = GreenARGB,
+      lines = Some(ScatterStyle.LineData(data0.indices.flatMap(i => Array(i, i)): PlotIntSeries))
+    )
+    for (file <- dataFiles; if FileRegex.findFirstIn(file.getName()).nonEmpty) {
       val data = CartAndRad.read(file)
-      val rads = data.map(_.rad)
-      val plot = Plot.simple(
+      val rads = data.map(p => if (p.rad > 5e-9) 6 else 2)
+      val plot = Plot.stacked(Seq(
+        scatter0,
+        vector0,
         ScatterStyle(
           data.map(_.x),
           data.map(_.y),
           symbolWidth = rads, 
           symbolHeight = rads,
-          xSizing = PlotSymbol.Sizing.Scaled,
-          ySizing = PlotSymbol.Sizing.Scaled
+          // xSizing = PlotSymbol.Sizing.Scaled,
+          // ySizing = PlotSymbol.Sizing.Scaled,
         )
-      ) //.
-      //updatedAxis[NumericAxis]("x", na => na.copy(min = xaxisMin, max = xaxisMax, tickLabelInfo = na.tickLabelInfo.map(_.copy(numberFormat = labelFormat)))).
-      //updatedAxis[NumericAxis]("y", na => na.copy(min = yaxisMin, max = yaxisMax, tickLabelInfo = na.tickLabelInfo.map(_.copy(numberFormat = labelFormat))))
+      ))
+      .updatedAxis[NumericAxis]("x", na => na.numberFormat(labelFormat))
+      .updatedAxis[NumericAxis]("y", na => na.numberFormat(labelFormat))
+      .withText("label", PlotText(simDir.getName()), Bounds(0.5, 0.05, 0.4, 0.05))
       // SwingRenderer.saveToImage(
       //   plot,
       //   "output." + ("0" * (7 - fnum.length)) + fnum + ".png",
       //   width = 1200,
       //   height = 1200
       // )
-      SwingRenderer(plot, 600, 600, true)
+      val FileRegex(num) = file.getName()
+      SwingRenderer.saveToImage(plot, s"${simDir.getAbsolutePath()}/plotCart.${"0"*(5-num.length)+num}.png")
     }
   }
 
