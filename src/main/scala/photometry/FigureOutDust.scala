21e828cc245c2ad9e2eae407255fe5faf7daba45
Mark Lewis <mlewis@trinity.edu>
2021-03-10 16:38:48 -0600

    Updated plots for dust stuff.

--- src/main/scala/photometry/FigureOutDust.scala
+++ src/main/scala/photometry/FigureOutDust.scala
@@ -5,6 +5,7 @@ import swiftvis2.plotting._
 import swiftvis2.plotting.styles.ScatterStyle
 import swiftvis2.plotting.renderer.SwingRenderer
 import java.io.File
+import util.GCCoord
 
 object FigureOutDust extends App {
   val labelFormat = "%1.3e"
@@ -25,27 +26,52 @@ object FigureOutDust extends App {
       GreenARGB
     )
     val vector0 = ScatterStyle(
-      data0.flatMap(p => Array(p.x, p.x + p.vx)),
-      data0.flatMap(p => Array(p.y, p.y + p.vy)),
+      data0.filter(_.rad > 5e-9).flatMap(p => Array(p.x, p.x + p.vx)),
+      data0.filter(_.rad > 5e-9).flatMap(p => Array(p.y, p.y + p.vy)),
       NoSymbol,
       colors = GreenARGB,
       lines = Some(ScatterStyle.LineData(data0.indices.flatMap(i => Array(i, i)): PlotIntSeries))
     )
     for (file <- dataFiles; if FileRegex.findFirstIn(file.getName()).nonEmpty) {
       val data = CartAndRad.read(file)
+      val gcdata = data.map(p => GCCoord(p))
       val rads = data.map(p => if (p.rad > 5e-9) 6 else 2)
-      val plot = Plot.stacked(Seq(
-        scatter0,
-        vector0,
-        ScatterStyle(
-          data.map(_.x),
-          data.map(_.y),
-          symbolWidth = rads, 
-          symbolHeight = rads,
-          // xSizing = PlotSymbol.Sizing.Scaled,
-          // ySizing = PlotSymbol.Sizing.Scaled,
+      val plot = Plot.stackedGridNN(Seq(Seq(
+        Seq(
+          scatter0,
+          vector0,
+          ScatterStyle(
+            data.map(_.x),
+            data.map(_.y),
+            symbolWidth = rads, 
+            symbolHeight = rads,
+          )
+        ),
+        Seq(
+          ScatterStyle(
+            (data, gcdata).zipped.flatMap((c, gc) => Array(c.x, gc.X)),
+            (data, gcdata).zipped.flatMap((c, gc) => Array(c.y, gc.Y)),
+            NoSymbol,
+            colors = YellowARGB,
+            lines = Some(ScatterStyle.LineData(data0.indices.flatMap(i => Array(i, i)): PlotIntSeries))
+          ),
+          ScatterStyle(
+            gcdata.map(_.X),
+            gcdata.map(_.Y),
+            symbolWidth = rads, 
+            symbolHeight = rads,
+            colors = RedARGB,
+          ),
+          ScatterStyle(
+            data.map(_.x),
+            data.map(_.y),
+            symbolWidth = data.map(_.rad * 2), 
+            symbolHeight = data.map(_.rad * 2),
+            xSizing = PlotSymbol.Sizing.Scaled,
+            ySizing = PlotSymbol.Sizing.Scaled,
+          )
         )
-      ))
+      )))
       .updatedAxis[NumericAxis]("x", na => na.numberFormat(labelFormat))
       .updatedAxis[NumericAxis]("y", na => na.numberFormat(labelFormat))
       .withText("label", PlotText(simDir.getName()), Bounds(0.5, 0.05, 0.4, 0.05))
@@ -56,7 +82,7 @@ object FigureOutDust extends App {
       //   height = 1200
       // )
       val FileRegex(num) = file.getName()
-      SwingRenderer.saveToImage(plot, s"${simDir.getAbsolutePath()}/plotCart.${"0"*(5-num.length)+num}.png")
+      SwingRenderer.saveToImage(plot, s"${simDir.getAbsolutePath()}/plotCart.${"0"*(5-num.length)+num}.png", width = 1600, height = 800)
     }
   }
 
